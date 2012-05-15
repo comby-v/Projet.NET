8f3932ebab8b8204c9422a8452866b78eb2088a8
Mika <Eworky@.(none)>
2012-05-15 15:47:44 +0200

    add  controllers, add dataAccess directory and 2 models

---
+++ ConcertFinder/ConcertFinderMVC/Controllers/EventController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConcertFinderMVC.Controllers
+{
+    public class EventController : Controller
+    {
+        //
+        // GET: /Event/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

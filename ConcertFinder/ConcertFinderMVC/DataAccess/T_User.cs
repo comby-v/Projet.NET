4884268c8ca803cc6f93d84d29ad0daeba3960a3
yoan <durand.yoan@gmail.com>
2012-05-15 17:32:45 +0200

    merge

--- ConcertFinder/ConcertFinderMVC/DataAccess/T_User.cs
+++ ConcertFinder/ConcertFinderMVC/DataAccess/T_User.cs
@@ -7,5 +7,19 @@ namespace ConcertFinderMVC.DataAccess
 {
     public class T_User
     {
+        public static Boolean create(USER user)
+        {
+            using (ConcertFinderEntities1 concert = new ConcertFinderEntities1())
+            {
+                long id = user.USER_ID;
+
+                concert.AddToUSERs(user);
+                concert.SaveChanges();
+
+                USER us = concert.USERs.Where(u => u.USER_ID == user.USER_ID).FirstOrDefault();
+
+                return (us.USER_ID == id);
+            }
+        }
     }
 }

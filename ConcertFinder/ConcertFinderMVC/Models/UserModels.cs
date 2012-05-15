ee6da917a54a853f2818794622ae83e373e73857
Mika <mika7869@gmail.com>
2012-05-15 19:23:25 +0200

    prepare some models for user and add controller methods

--- ConcertFinder/ConcertFinderMVC/Models/UserModels.cs
+++ ConcertFinder/ConcertFinderMVC/Models/UserModels.cs
@@ -2,10 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace ConcertFinderMVC.Models
 {
-    public class UserModels
+    public class RegisterModel
     {
+        [Required]
+        [Display(Name = "Nom")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Prénom")]
+        public string Firstname { get; set; }
+
+        [Required]
+        [Display(Name = "Pseudo")]
+        public string Pseudo { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Ville")]
+        public string City { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Le {0} doit faire au moins {2} caractères de long !", MinimumLength = 4)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmer le mot de passe")]
+        [Compare("Password", ErrorMessage = "La confirmation de mot de passe n'est pas identique.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LogOnModel
+    {
+        [Required]
+        [Display(Name = "Pseudo")]
+        public string Pseudo { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
     }
 }

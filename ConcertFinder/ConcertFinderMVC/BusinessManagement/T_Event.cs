81c32a13ca6aaf794228cf71d5f03cf2b4d7ca3d
Vincent Comby <vincent.comby@gmail.com>
2012-05-15 23:31:07 +0200

    fix little bug

---
+++ ConcertFinder/ConcertFinderMVC/BusinessManagement/T_Event.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ConcertFinderMVC.Models;
+
+namespace ConcertFinderMVC.BusinessManagement
+{
+    public class T_Event
+    {
+    /*    static public bool Create(EventItem myevent)
+        {
+            EventArgs 
+            DataAccess.T_Event.Create();
+        }
+
+        static public bool Delete(long id)
+        {
+
+        }
+
+
+
+        static public bool Update(EventItem myevent)
+        {
+
+        }
+
+        static public EventItem Get(long id)
+        {
+
+        }
+
+        static public List<EventItem> GetListEvent(int nbr, string type = "")
+        {
+
+        }
+
+        static public List<EventItem> GetListLastAddEvent(int nbr, string type = "")
+        {
+
+        }*/
+    }
+}

37c3b936e2f910b10c467998ad3c581082b27b9b
Vincent Comby <vincent.comby@gmail.com>
2012-05-18 01:26:24 +0200

    hitler robertls

---
+++ ConcertFinder/BackEndAdmin/DynamicData/FieldTemplates/Children.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="C#" CodeBehind="Children.ascx.cs" Inherits="BackEndAdmin.ChildrenField" %>
+
+<asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="<%# GetChildrenPath() %>" />
+
--- ConcertFinder/ConcertFinderMVC/BusinessManagement/T_Event.cs
+++ ConcertFinder/ConcertFinderMVC/BusinessManagement/T_Event.cs
@@ -3,33 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using ConcertFinderMVC.Models;
+using ConcertFinderMVC.DataAccess;
 
 namespace ConcertFinderMVC.BusinessManagement
 {
     public class T_Event
     {
-    /*    static public bool Create(EventItem myevent)
+        static public bool Create(FormEventModels myevent, USER user)
         {
-            EventArgs 
-            DataAccess.T_Event.Create();
+            EVENT ev = new EVENT ();
+            ev.EVENT_TITRE = myevent.Title;
+            ev.EVENT_TYPE = myevent.Type;
+            ev.EVENT_DESCRIPTION = myevent.Description;
+            ev.EVENT_DATEDEBUT = myevent.StartDate;
+            ev.EVENT_DATEFIN = myevent.EndDate;
+            ev.EVENT_IMG_PATH = myevent.Image;
+            ev.EVENT_EMAIL = myevent.Email;
+            ev.EVENT_SITE = myevent.Website;
+            ev.EVENT_TEL = myevent.Phone;
+
+
+            //ev.LOCATION = DataAccess.getMapCoord (ev.LOCATION);
+            /*
+             if (pas d'existant)
+             {
+                ev.LOCATION.LOCATION_PAYS = myevent.Country;
+                ev.LOCATION.LOCATION_VILLE = myevent.City;
+                ev.LOCATION.LOCATION_CP = myevent.CodePostal;
+                ev.LOCATION.LOCATION_RUE = myevent.Address;
+                ev.LOCATION.LOCATION_NAME = myevent.RoomName;
+                ev.USER = user;
+            }*/
+            return DataAccess.T_Event.Create(ev);
         }
 
         static public bool Delete(long id)
         {
-
+            return DataAccess.T_Event.Delete (id);
         }
 
 
 
-        static public bool Update(EventItem myevent)
+        static public bool Update(FormEventModels myevent, LOCATION location, USER user, long id)
         {
-
+            EVENT ev = new EVENT ();
+            ev.EVENT_ID = id;
+            ev.EVENT_TITRE = myevent.Title;
+            ev.EVENT_TYPE = myevent.Type;
+            ev.EVENT_DESCRIPTION = myevent.Description;
+            ev.EVENT_DATEDEBUT = myevent.StartDate;
+            ev.EVENT_DATEFIN = myevent.EndDate;
+            ev.EVENT_IMG_PATH = myevent.Image;
+            ev.EVENT_EMAIL = myevent.Email;
+            ev.EVENT_SITE = myevent.Website;
+            ev.EVENT_TEL = myevent.Phone;
+            ev.LOCATION = location;
+            ev.USER = user;
+            return DataAccess.T_Event.Update(ev);
         }
 
         static public EventItem Get(long id)
         {
 
-        }*/
+        }
 
         static public List<EventItem> GetListEvent(int nbr, string type = "")
         {

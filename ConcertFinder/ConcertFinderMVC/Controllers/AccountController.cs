df7dcfe1fe36d52dae6bd981e9dd017952c5a9c9
Vincent Comby <vincent.comby@gmail.com>
2012-05-18 19:16:07 +0200

    add AccountController

---
+++ ConcertFinder/ConcertFinderMVC/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConcertFinderMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: /Account/LogOn
+
+        [HttpPost]
+        public ActionResult LogOn(Models.LogOnModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+              /*  if (Membership.ValidateUser(model.Pseudo, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);*/
+                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Event");
+                    }
+            }
+          // If we got this far, something failed, redisplay form
+           return View(model);
+
+        }
+    }
+}

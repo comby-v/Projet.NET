d5baff117423835ba348da227ba2a875f4dff387
Mika <mika7869@gmail.com>
2012-05-24 19:05:15 +0200

    add missing files

---
+++ ConcertFinder/ConcertFinderMVC/BusinessManagement/Tool.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ConcertFinderMVC.DataAccess;
+
+namespace ConcertFinderMVC.BusinessManagement
+{
+    public class Tool
+    {
+        public static string Truncate(string description)
+        {
+            return description.ToArray().Take(100).ToString();
+        }
+
+        public static bool IsAdmin(string pseudo)
+        {
+            using (ConcertFinderEntities bdd = new ConcertFinderEntities())
+            {
+                try
+                {
+                    return (bdd.T_User.Where(u => u.Pseudo == pseudo).FirstOrDefault().Role.Equals("Administrateur"));
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ ConcertFinder/ConcertFinderMVC/DataAccess/T_Event.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ConcertFinderMVC.DataAccess
+{
+    public partial class T_Event : IEquatable<T_Event>
+    {
+        public bool Equals(T_Event evnt)
+        {
+            //Check whether the compared object is null.
+            if (Object.ReferenceEquals(evnt, null)) return false;
+
+            //Check whether the compared object references the same data.
+            if (Object.ReferenceEquals(this, evnt)) return true;
+
+            //Check whether the products' properties are equal.
+            return Id.Equals(evnt.Id);
+        }
+
+        // If Equals() returns true for a pair of objects 
+        // then GetHashCode() must return the same value for these objects.
+
+        public override int GetHashCode()
+        {
+            //Get hash code for the Id field.
+            int hashIdCode = Id.GetHashCode();
+
+            //Calculate the hash code for the event.
+            return hashIdCode;
+        }
+    }
+}
